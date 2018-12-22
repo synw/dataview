aabc156c8b6109a84aa880eb1371cb68397601be
Win <synwe@yahoo.com>
2018-12-22 01:40:12 +0000

    Add a better example

--- example/main.dart
+++ example/main.dart
@@ -1,18 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:dataview/dataview.dart';
 
-final routes = {
-  // ...
-  '/dataview': (BuildContext context) => new DataviewScreen("/"),
-};
-
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'My app',
-      routes: routes,
+      home: HomePage(),
     );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: RaisedButton(
+            onPressed: () {
+              Navigator.of(context).push(MaterialPageRoute(
+                builder: (context) {
+                  return DataviewPage("/");
+                },
+              ));
+            },
+            child: Text("View application data")));
   }
 }
 
